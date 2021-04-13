8c431d3122ba62b8eaf05d7845dd2f92a49e1e63
user <user@DESKTOP-7JHN3KB>
2021-04-13 09:12:00 +0900

    SecondClass 추가 + FirstClass print 추가 작성

--- Test20210413/src/first/SecondClass.java
+++ Test20210413/src/first/SecondClass.java
@@ -1,11 +1,9 @@
 package first;
 
-public class FirstClass {
+public class SecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git!");
-		System.out.println("첫번째 클래스");
 
 	}
 
